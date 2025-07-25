9ce51a6e7d886ff231080ce83d13233b18390115
Nguyen Phan <nqphan212@gmail.com>
2025-07-25 09:25:11 -0400

    Structure project using clean architecture and add Product/Category entities

---
+++ ECommerce.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Domain.Common;
+
+namespace ECommerce.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    // Navigation property
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
---
+++ ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Common;
+
+namespace ECommerce.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+
+    // Foreign key
+    public Guid CategoryId { get; set; }
+
+    // Navigation property
+    public Category? Category { get; set; }
+}
